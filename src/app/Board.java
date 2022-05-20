19878cff157e1dc7ddfdd6f3073b3f0cb808d9d6
Matt Rogan <mr1360@york.ac.uk>
2022-05-20 21:13:47 +0100

    Created Board.java object

---
+++ src/app/Board.java
@@ -0,0 +1,20 @@
+package app;
+
+import java.util.ArrayList;
+
+public class Board
+{
+
+    Card[][] positions = new Card[4][4];
+
+    public Board(){}
+
+    /**
+     * @param deck Deck of cards to place from
+     */
+    public void placeInitialCards(ArrayList<Card> deck){
+
+    }
+
+
+}
